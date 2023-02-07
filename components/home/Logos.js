aa207582e71af006624f5c88a57361c995624b66
mtjako <mateusz@jakowlew.pl>
2023-02-07 13:13:59 +0100

    ...

--- components/home/Logos.js
+++ components/home/Logos.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
-import styles from "./styles.modules.scss";
+import styled from "styled-components";
 const logos = ["", "", "", "", "", "", "", "", "", ""];
 export const Logos = () => {
   const logsDiv = useRef(null);
@@ -29,24 +29,81 @@ export const Logos = () => {
   }, [scrollPosition]);
 
   return (
-    <div className={styles.logos}>
-      <h2>Naprawiamy sprzęt marek</h2>
-      <div className={styles.tilesWrap}>
-        <div className={styles.tiles} ref={logsDiv}>
-          {[...logos, ...logos].map((logo, index) => (
-            <div key={index} className={styles.tilesTile}>
-              <Image
-                src={`/brands/logo${
-                  index + 1 <= 9 ? index + 1 : index + 1 - 9
-                }.png`}
-                width={200}
-                height={56}
-                alt=""
-              />
-            </div>
-          ))}
+    <XD>
+      <div className={"logos"}>
+        <h2>Naprawiamy sprzęt marek</h2>
+        <div className={"tilesWrap"}>
+          <div className={"tiles"} ref={logsDiv}>
+            {[...logos, ...logos].map((logo, index) => (
+              <div key={index} className={"tilesTile"}>
+                <Image
+                  src={`/brands/logo${
+                    index + 1 <= 9 ? index + 1 : index + 1 - 9
+                  }.png`}
+                  width={200}
+                  height={56}
+                  alt=""
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </XD>
   );
-}
+};
+
+const XD = styled.div`
+  .logos {
+    padding: 80px 0;
+  }
+  .logos h2 {
+    font-weight: 700;
+    font-size: 40px;
+    text-align: center;
+    margin: 0 0 48px;
+  }
+  .tiles {
+    display: flex;
+    overflow-x: scroll;
+  }
+  .tiles::-moz-scrollbar {
+    display: none;
+  }
+  .tiles::-webkit-scrollbar {
+    display: none;
+  }
+  .tilesTile {
+    min-width: 200px;
+    width: 200px;
+    height: 56px;
+
+    margin-right: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: 700;
+    font-size: 32px;
+  }
+  .tilesTitle:last-child {
+    margin-right: 0;
+  }
+  .tilesWrap {
+    position: relative;
+  }
+  .tilesWrap::after {
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    right: 0;
+    background: linear-gradient(
+      90deg,
+      rgba(250, 250, 249, 1) 0%,
+      rgba(250, 250, 249, 0) 25%,
+      rgba(250, 250, 249, 0) 75%,
+      rgba(250, 250, 249, 1) 100%
+    );
+  }
+`;
